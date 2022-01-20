aa6fe2ab1d2b9b56f42e5c938c2edcbb88138665
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-20 18:47:55 +0100

    add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import '../styles/App.scss';
+import callToApi from '../services/api';
+import ls from '../services/localStorage';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
