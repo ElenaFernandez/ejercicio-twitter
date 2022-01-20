ff8d5f9cea19f0eed4fc45368227e273e8b3895d
ElenaFernandez <elena.fernandez92@gmail.com>
2022-01-20 18:49:48 +0100

    migrate Html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 // Fichero src/index.js (código nuevo)
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
